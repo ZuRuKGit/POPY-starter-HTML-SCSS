40f2c79c92d831a90a9cb91da28c40dab6b3572e
sutterlity <laurent@sutterlity.fr>
2013-07-15 21:02:37 +0200

    Mise à jour v1.4

--- config.rb
+++ config.rb
@@ -1,21 +1,21 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
-sass_dir = "scss"
-images_dir = "img"
-javascripts_dir = "js"
-fonts_dir = "fonts"
+http_path       = "/"
+css_dir         = "assets/css"
+sass_dir        = "assets/scss"
+images_dir      = "assets/img"
+javascripts_dir = "assets/js"
+fonts_dir       = "assets/fonts"
 
-output_style = :expanded
-environment = :production
+output_style    = :expanded
+environment     = :production
 
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
-color_output = false
+color_output    = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
